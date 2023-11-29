584155f0c951556034bc53fe59546ef52cf9b8ac
Felipe Vidal <felipe.vidal@mesquita.rj.gov.br>
2023-11-29 09:52:04 -0300

    UI

--- frontend/src/components/partials/SidebarLink.tsx
+++ frontend/src/components/partials/SidebarLink.tsx
@@ -23,7 +23,9 @@ export default function SidebarLink({
           sidebarIsOpen
             ? "w-[80px] flex-col p-2 text-xs md:w-[208px] md:flex-row md:px-2 md:py-4"
             : "w-[80px] flex-col p-2 text-xs"
-        } items-center ${isActive ? "bg-slate-300" : "hover:bg-slate-400"}`
+        } items-center ${
+          isActive ? "bg-slate-300 hover:bg-slate-200" : "hover:bg-slate-400"
+        }`
       }
     >
       {logo}{" "}
