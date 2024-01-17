212b5aff4895509e3adbe83cb1d9ed0c245f740c
Felipe Vidal <felipe.vidal@mesquita.rj.gov.br>
2024-01-17 15:22:09 -0300

    Confirmation Dialog

--- frontend/src/components/ui/ConfirmationDialog.tsx
+++ frontend/src/components/ui/ConfirmationDialog.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { useAtom } from "jotai";
 import { notificationAtom } from "@/store";
+import { LoaderIcon } from "lucide-react";
 
 interface ConfirmationDialogProps {
   message: string;
@@ -36,22 +37,28 @@ export default function ConfirmationDialog({
         onClick={reject}
       ></div>
       <div className="fixed left-1/2 top-1/2 z-50 flex max-h-[90vh] w-72 max-w-[90vw] translate-x-[-50%] translate-y-[-50%] flex-col gap-3 overflow-auto rounded bg-slate-100 p-4 sm:w-[30rem] md:w-[36rem] lg:w-[42rem]">
-        <h2 className="text-center text-white/90">{message}</h2>
+        <h2 className="text-center text-slate-900">{message}</h2>
         <div className="flex w-full gap-4">
-          <button
-            onClick={handleAccept}
-            disabled={isLoading}
-            className="flex-1 rounded bg-slate-500/40 bg-gradient-to-r px-4 py-1 text-white text-white/80 shadow shadow-black/20 hover:bg-slate-500/75 hover:text-white disabled:bg-slate-500/10"
-          >
-            {isLoading ? "Carregando..." : "CONFIRMAR"}
-          </button>
-          <button
-            onClick={reject}
-            disabled={isLoading}
-            className="flex-1 rounded bg-slate-400 px-4 py-1 text-white hover:bg-slate-500 disabled:bg-slate-400/25"
-          >
-            CANCELAR
-          </button>
+          {isLoading ? (
+            <LoaderIcon className="mx-auto animate-spin text-slate-700 duration-2000" />
+          ) : (
+            <>
+              <button
+                onClick={handleAccept}
+                disabled={isLoading}
+                className="flex-1 rounded bg-green-600 bg-gradient-to-r px-4 py-1 text-white shadow shadow-black/20 hover:bg-green-700 hover:text-white disabled:bg-slate-500/10"
+              >
+                {isLoading ? "Carregando..." : "CONFIRMAR"}
+              </button>
+              <button
+                onClick={reject}
+                disabled={isLoading}
+                className="flex-1 rounded bg-slate-400 px-4 py-1 text-white hover:bg-slate-500 disabled:bg-slate-400/25"
+              >
+                CANCELAR
+              </button>
+            </>
+          )}
         </div>
       </div>
     </>,
